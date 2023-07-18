c2e23589f3e6f6201ac5983236be5725ec7c9ce1
DamianT <elmo@DESKTOP-OP1RDAI>
2023-07-17 19:31:38 -0600

    testing icons

--- src/components/post-view/Left-aside.jsx
+++ src/components/post-view/Left-aside.jsx
@@ -3,19 +3,19 @@ export default function Leftaside() {
     <div className="flex flex-col mt-10 me-5 gap-5">
       <div>
         <img
-          src="../src/assets/icons/love.svg"
+          src="../love.svg"
           alt="heart-reactions icon"
           className="w-6 h-6 "
         />
         <p className="text-center">4</p>
       </div>
       <div>
-        <img src="../src/assets/icons/comments-count.svg" alt="comments icon" />
+        <img src="../comments-count.svg" alt="comments icon" />
         <p className="text-center">0</p>
       </div>
       <div>
         <img
-          src="../src/assets/icons/save-No-Background-Icon.svg"
+          src="../save-No-Background-Icon.svg"
           alt="comments icon"
           className="w-6 h-6"
         />
